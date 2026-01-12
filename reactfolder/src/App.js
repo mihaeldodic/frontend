493d9c6d9f1a487689417f890a912599d5eeb495
mihaeldodic <mihaeldodic77@gmail.com>
2026-01-12 19:52:02 +0100

    intial commit

---
+++ reactfolder/src/App.js
@@ -0,0 +1,16 @@
+import Test from './components/Test';
+import './App.css';
+import Slika from './components/Slika';
+import './components/Slika.css';
+
+function App() {
+  return (
+    <>
+      <Test />
+      <Slika />
+      <h2>Drugi naslov</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ reactfolder/src/components/Slika.js
@@ -0,0 +1,9 @@
+import "./Slika.css"
+
+const Slika = () => {
+    return (
+        <img className="slika" src="https://storage.googleapis.com/pod_public/800webp/239543.webp" alt="Random" />
+    )
+}
+
+export default Slika;
