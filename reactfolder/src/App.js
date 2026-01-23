b414bcd0df42e0806447497556c46645b1e597f0
Mihael Dodic <mihaeldodic77@gmail.com>
2026-01-23 17:29:46 +0100

    json vjezba

--- reactfolder/src/App.js
+++ reactfolder/src/App.js
@@ -6,6 +6,10 @@ import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import Profil from "./components/zadaci/Profil";
+import Korisnici from "./components/zadaci/Korisnici";
+import Vjezbadva from "./components/Vjezbadva"; 
+import Tecaj from "./components/zadaci/Tecaj";
 
 function App() {
   return (
@@ -14,6 +18,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog" element={<Blog />} />
+          <Route path="/profil" element={<Profil />} />
+          <Route path="/korisnici" element={<Korisnici />} />
+          <Route path="/vjezbadva" element={<Vjezbadva />} />
+          <Route path="/tecaj" element={<Tecaj />} />
+
         </Routes>
       <Footer />
     </BrowserRouter>
--- reactfolder/src/pages/Blog.js
+++ reactfolder/src/pages/Blog.js
@@ -1,14 +1,31 @@
-
+import "./Blog.css"
+import posts from "../components/zadaci/data/blog.json";
 
 const Blog = () => {
     return (
-    <>
-   
-        <h1>Blog</h1>
-        <h1>Welcome to our Blog Page!</h1>
-   
-    </>
-    )
-}
+    <div className="blog-post">
+        <div className="container">
+            <h1>Blog</h1>
+            <div className="row">
+                {posts.map((post) => (
+                    <div className="col-md-4 mb-4">
+                        <img src={post._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}/>
+                        <h2>{post.title.rendered}</h2>
+                        <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered}}></div>
+                        
+                        <p>{post._embedded.author[0].name} | vrijeme objave {new Date(post.date).toLocaleString("hr-HR", {
+                                day: "2-digit",
+                                month: "2-digit",
+                                year: "numeric",
+                                hour: "2-digit",
+                                minute: "2-digit"
+                                })}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+    );
+};
 
 export default Blog;
