301a1afc7845929080eec4ea8d2677da73ac2126
Mihael Dodic <mihaeldodic77@gmail.com>
2026-02-09 20:45:38 +0100

    singup

--- reactfolder/src/components/SignIn.js
+++ reactfolder/src/components/SignIn.js
@@ -1,42 +1,100 @@
-import "./signin.css"
-
+import { Link, Navigate } from "react-router-dom";
+import "./signin.css";
+import { useState } from "react";
 
 const SignIn = () => {
-    return (
-        <div className="container">
-            <div className="row">
-                
-                <div className="d-done d-md-flex col-md-6 d-flex profile-left">
-                    <h2>Welcome</h2>
-                </div>
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-                <div className="col-md-6 profile-right">
-                     <h2>Sign In</h2>
-                    <form className="signin-form">
-                       
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  });
 
-                        <input
-                            type="text"
-                            placeholder="Username"
-                            className="form-control"
-                        />
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-                        <input
-                            type="password"
-                            placeholder="Lozinka"
-                            className="form-control"
-                        />
-                        <a href="#">Zaboravili ste lozinku?</a>
+  console.log(form);
 
-                        <button type="submit" className="btn btn-primary">
-                            Prijavi se
-                        </button>
-                    </form>
-                </div>
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
 
-            </div>
+    try {
+      const response = await fetch(
+        "https://front2.edukacija.online/backend/wp-json/jwt-auth/v1/token",
+        {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(form),
+        },
+      );
+
+      const data = await response.json();
+
+      setLoading(false);
+
+      console.log(data);
+
+      if (data?.code) {
+        setError("Wrong Email or password");
+        return;
+      }
+
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("username", data.user_display_name);
+
+      window.location.reload();
+    } catch (error) {
+      setLoading(false);
+      setError("Something went wrong. Please try again.");
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="d-none d-md-flex col-md-6 profile-left">
+          <h1>ENJOY THE</h1>
+          <img src="" alt="" />
         </div>
-    );
+        <div className="col-md-6 profile-right">
+          <h2>Welcome back</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <form onSubmit={handleLogin} className="signin-form">
+            <label htmlFor="">Username</label>
+            <input
+              type="text"
+              name="username"
+              value={form.username}
+              onChange={handleChange}
+            />
+            <label htmlFor="">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+            />
+            <a href="#">Forgot password?</a>
+            <button type="submit">Log in</button>
+            <p>{error}</p>
+          </form>
+
+          <p className="text-center breakline">or</p>
+          <p>
+            Don't have an account? <Link>Sign Up</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default SignIn
+export default SignIn;
